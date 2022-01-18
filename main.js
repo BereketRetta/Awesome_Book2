a2651a2f987595f8969c97dd33867288e7691b54
Bereket Retta <bereket@etmsoftwareplc.com>
2022-01-18 11:06:34 +0300

    fix js file

--- main.js
+++ main.js
@@ -1,146 +1,74 @@
-const list = document.getElementById('add-book');
-const form = document.getElementById('book-entry');
+import {
+  renderBooks,
+  titleInput,
+  authorInput,
+  addBook,
+  localBooks,
+} from './modules/variables.js';
 
-const addBooks = 'addBooks';
-const removeBooks = 'removeBooks';
-const loadData = 'loadData ';
+// import { DateTime } from './node_modules/luxon/build/es6/luxon.js';
+import renderBook from './modules/render.js';
+import setStore from './modules/setStore.js';
 
-const createId = () => {
-  return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
-}
+const { luxon } = window;
+const date = document.getElementById('date');
+date.innerText = luxon.DateTime.now().toFormat('MMMM dd, yyyy, hh:mm:ss a').toString();
 
-const createStore = (books = []) => {
-  let state = books;
-  const contentUpdate = [];
+let books = [];
 
-  const update = (action) => {
-    if (action.type === addBooks) {
-      state = state.concat([action.book]);
-    } else if (action.type === removeBooks) {
-      state = state.filter((book) => book.id !== action.id);
-    } else if (action.type === loadData) {
-      state = action.books;
-    }
-    contentUpdate.forEach((fn) => fn());
-  };
+const getStore = (localBooks) => {
+  if (localBooks) {
+    books = JSON.parse(localBooks);
+  }
+};
 
-  const getState = () => state;
-
-  const onUpdate = (fn) => contentUpdate.push(fn);
-
-  return {
-    update,
-    getState,
-    onUpdate,
-  };
-}
-
-class BookStore {
-  constructor() {
-    const store = createStore();
-    store.onUpdate(() => {
-      localStorage.setItem('saved-data', JSON.stringify(store.getState()));
-    });
-    this.store = store;
+class Book {
+  constructor(title, author) {
+    this.id = Math.random().toString(36).substring(2, 7);
+    this.title = title;
+    this.author = author;
   }
 
-  get books() {
-    return this.store.getState();
-  }
-
-  addBook(book) {
-    this.store.update({
-      type: addBooks,
-      book,
-    });
-  }
-
-  removeBook(id) {
-    this.store.update({
-      type: removeBooks,
-      id,
-    });
-  }
-
-  onUpdate(func) {
-    this.store.onUpdate(func);
-  }
-
-  loadBooks() {
-    const saved = localStorage.getItem('saved-data');
-    if (saved) {
-      this.store.update({
-        type: loadData,
-        books: JSON.parse(saved),
-      });
+  static addItem(title, author) {
+    if (title !== '' && author !== '') {
+      const newBook = new Book(title, author);
+      books.push(newBook);
     }
   }
+
+  static deleteBook(id) {
+    books = books.filter((book) => book.id !== id);
+  }
 }
 
-const bookStore = new BookStore();
-
-form.addEventListener('submit', (event) => {
+const addNewBook = (event) => {
   event.preventDefault();
-  const title = form.elements[0].value;
-  const author = form.elements[1].value;
-  const id = createId();
-  form.reset();
-  bookStore.addBook({ title, author, id });
-});
-
-const addBookToDOM = (book) => {
-  const node = document.createElement('li');
-  const bookDetails = document.createElement('div');
-  const title = document.createElement('h2');
-  title.innerText = book.title;
-
-  const subtitle = document.createElement('p');
-  subtitle.innerText = `by  ${book.author}`;
-
-  const button = document.createElement('button');
-  button.innerText = 'Remove';
-
-  button.addEventListener('click', () => bookStore.removeBook(book.id));
-
-  bookDetails.appendChild(title);
-  bookDetails.appendChild(subtitle);
-  node.appendChild(bookDetails);
-  node.appendChild(button);
-
-  list.appendChild(node);
-}
-
-bookStore.onUpdate(() => {
-  list.innerHTML = '';
-  bookStore.books.forEach(addBookToDOM);
-});
-
-window.addEventListener('load', () => {
-  // eslint-disable-next-line no-undef
-  const { DateTime } = luxon;
-  const now = DateTime.now();
-  document.getElementById('currentDateTime').innerText = now.toLocaleString(DateTime.DATETIME_MED);
-  bookStore.loadBooks();
-});
-
-const links = document.querySelectorAll('.link');
-
-links.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelector('.active').classList.remove('active');
-    link.classList.add('active');
-    const id = link.getAttribute('href').slice('1');
-
-    const currentSection = document.querySelector('.active-page');
-    if (currentSection) {
-      currentSection.classList.remove('active-page');
+  Book.addItem(titleInput.value, authorInput.value);
+  setStore(books);
+  titleInput.value = '';
+  authorInput.value = '';
+  renderBook(renderBooks, books);
+};
+const deleteBook = (e) => {
+  Book.deleteBook(e.target.rel);
+  setStore(books);
+  renderBook(renderBooks, books);
+};
+addBook.onclick = addNewBook;
+renderBooks.onclick = deleteBook;
+getStore(localBooks);
+renderBook(renderBooks, books);
+const a = document.querySelectorAll('.links a');
+a.forEach((e) => {
+  e.onclick = () => {
+    if (e.classList.contains('active')) {
+      return;
     }
-    const section = document.getElementById(id);
-    if (section) {
-      section.classList.add('active-page');
-    }
-  });
+    a.forEach((i) => i.classList.remove('active'));
+    e.classList.add('active');
+    const showMe = document.querySelector('.show-me');
+    const divClass = document.querySelector(`.${e.id}`);
+    showMe.classList.remove('show-me');
+    divClass.classList.add('show-me');
+  };
 });
